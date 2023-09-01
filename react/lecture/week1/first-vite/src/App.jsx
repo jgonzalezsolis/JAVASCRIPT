a8eda7011d5d8174dbeaabcf6975fabff8393d93
jgonzalezsolis <jose_5lic@hotmail.com>
2023-09-01 01:09:11 -0600

    first

---
+++ react/lecture/week1/first-vite/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h2>Hello React and Vite and Vites</h2>
+      <p>Hello '''''''ddsd' React and Vite and Vites</p>
+    </>
+  )
+}
+
+export default App
